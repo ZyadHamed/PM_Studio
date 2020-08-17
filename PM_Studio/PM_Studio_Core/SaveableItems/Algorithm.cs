89bffc1a7d22aeb6f0911e79eae957bab2b199a2
ZyadHamed <zyadhamedashour@gmail.com>
2020-08-17 12:07:45 +0200

    Added a new Savable type(Algorithm)

---
+++ PM_Studio/PM_Studio_Core/SaveableItems/Algorithm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM_Studio
+{
+    public class Algorithm
+    {
+        public string algorithmFileName { get; set; }
+        public string algorithm { get; set; }
+    }
+}
