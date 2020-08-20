c4db101e48c56518234ab304376fb5e4daa038ef
ZyadHamed <zyadhamedashour@gmail.com>
2020-08-20 23:09:46 +0200

    Added the Node System Savable Item

---
+++ PM_Studio/PM_Studio_Core/SaveableItems/NodeSystem.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM_Studio
+{
+    public class NodeSystem
+    {
+        public List<string> NodesText { get; set; }
+        
+    }
+}
