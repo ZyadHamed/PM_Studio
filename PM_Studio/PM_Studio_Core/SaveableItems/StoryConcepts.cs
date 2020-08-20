73d459ffe831b7c28bb5f7262ffe971a84bbb714
ZyadHamed <zyadhamedashour@gmail.com>
2020-08-20 11:21:21 +0200

    Added the StoryConcepts Savable Class

---
+++ PM_Studio/PM_Studio_Core/SaveableItems/StoryConcepts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM_Studio
+{
+    [Serializable]
+    class StoryConcepts
+    {
+        public string[] StoryTopics { get; set; }
+        public string[] StoryTypes { get; set; }
+        public string[] PlotTwists { get; set; }
+        public string[] PlotPoints { get; set; }
+        public string[] StoryMinorSituations { get; set; }
+    }
+}
