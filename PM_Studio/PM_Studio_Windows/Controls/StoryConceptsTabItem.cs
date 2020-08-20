665509794f80422532b883b4d540e3546a4de3cb
ZyadHamed <zyadhamedashour@gmail.com>
2020-08-20 11:19:39 +0200

    Added the story concepts Tab Item

---
+++ PM_Studio/PM_Studio_Windows/Controls/StoryConceptsTabItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace PM_Studio
+{
+    class StoryConceptsTabItem : FileTabItem
+    {
+        public StoryConceptsTabItem(TabControl tabControl, string header, string filePath) : base(tabControl, header, filePath)
+        {
+    
+        }
+    }
+}
