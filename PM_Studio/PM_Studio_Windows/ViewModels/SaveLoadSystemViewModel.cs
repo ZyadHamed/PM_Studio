73fa03d30100ffab04b7b563c2eac8516f3bfc7b
ZyadHamed <zyadhamedashour@gmail.com>
2020-08-21 21:27:22 +0200

    Updated the algorithm returning method in SaveLoadSystemViewModel
    Updated the algorithm returning method in SaveLoadSystemViewModel to match the new data loading system for the AlgorithmTabItem

--- PM_Studio/PM_Studio_Windows/ViewModels/SaveLoadSystemViewModel.cs
+++ PM_Studio/PM_Studio_Windows/ViewModels/SaveLoadSystemViewModel.cs
@@ -23,7 +23,7 @@ namespace PM_Studio
             //Get the algorithm file from the given path and return it as a Class again 
             var Algorithm =  SaveLoadSystem.LoadData<Algorithm>(filePath);
             //Construct an Algorithm Editing tab using the information inside that Algorithm class,(and remove any stars at the file name)
-            return new AlgorithmTabItem(null, Algorithm.algorithm, Algorithm.algorithmFileName.Replace("*",""), filePath);
+            return new AlgorithmTabItem(null, Algorithm, filePath);
         }
 
        public StoryConcepts ReturnStoryConcepts(string filePath)
