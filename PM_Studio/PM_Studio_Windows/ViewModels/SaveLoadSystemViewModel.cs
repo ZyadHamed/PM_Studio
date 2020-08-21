565aff2510225981801976a9a30dc78b32f2669d
ZyadHamed <zyadhamedashour@gmail.com>
2020-08-21 21:05:09 +0200

    Added the StoryConcepts Returning Method

--- PM_Studio/PM_Studio_Windows/ViewModels/SaveLoadSystemViewModel.cs
+++ PM_Studio/PM_Studio_Windows/ViewModels/SaveLoadSystemViewModel.cs
@@ -18,11 +18,27 @@ namespace PM_Studio
             SaveLoadSystem.SaveData<T>(filePath, objectToSave);
         }
 
-        public Algorithm ReturnAlgorithm(string filePath)
+        public AlgorithmTabItem ReturnAlgorithm(string filePath)
         {
-            return SaveLoadSystem.LoadData<Algorithm>(filePath);
+            //Get the algorithm file from the given path and return it as a Class again 
+            var Algorithm =  SaveLoadSystem.LoadData<Algorithm>(filePath);
+            //Construct an Algorithm Editing tab using the information inside that Algorithm class,(and remove any stars at the file name)
+            return new AlgorithmTabItem(null, Algorithm.algorithm, Algorithm.algorithmFileName.Replace("*",""), filePath);
         }
 
+       public StoryConcepts ReturnStoryConcepts(string filePath)
+       {
+            return SaveLoadSystem.LoadData<StoryConcepts>(filePath);
+       }
+
+        public StoryConceptsTabItem ReturnStoryConceptsTabItem(string filePath)
+        {
+            //Get the StoryConcepts file inside that given path
+            StoryConcepts storyConcepts = ReturnStoryConcepts(filePath);
+            //Construct a StoryConcepts TabItem using the information in that file
+            return new StoryConceptsTabItem(null, filePath, storyConcepts);
+        }
+        
         #endregion
     }
 }
