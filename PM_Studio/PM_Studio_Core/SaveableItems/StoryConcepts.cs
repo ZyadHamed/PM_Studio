9e398c852e19f19eef6f8cb5cfe26d6ef05d0398
ZyadHamed <zyadhamedashour@gmail.com>
2020-08-21 18:16:57 +0200

    Updated most of properties from Arrays to normal strings

--- PM_Studio/PM_Studio_Core/SaveableItems/StoryConcepts.cs
+++ PM_Studio/PM_Studio_Core/SaveableItems/StoryConcepts.cs
@@ -5,12 +5,12 @@ using System.Text;
 namespace PM_Studio
 {
     [Serializable]
-    class StoryConcepts
+    public class StoryConcepts
     {
         public string[] StoryTopics { get; set; }
-        public string[] StoryTypes { get; set; }
-        public string[] PlotTwists { get; set; }
-        public string[] PlotPoints { get; set; }
-        public string[] StoryMinorSituations { get; set; }
+        public string StoryTypes { get; set; }
+        public string PlotTwists { get; set; }
+        public string PlotPoints { get; set; }
+        public string StoryEvents { get; set; }
     }
 }
