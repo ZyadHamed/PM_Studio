613f66ca4707a28e9a9b071fed7cdbc71a4ec3be
ZyadHamed <zyadhamedashour@gmail.com>
2020-08-24 01:34:09 +0200

    Added the collapsing left Panel button
    Added the collapsing left Panel button, with the feature of collapsing and uncollapsing the left panel

--- PM_Studio/PM_Studio_Windows/MainWindow.xaml.cs
+++ PM_Studio/PM_Studio_Windows/MainWindow.xaml.cs
@@ -20,9 +20,12 @@ namespace PM_Studio
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool IsPanelCollapsed = false;
         public MainWindow()
         {
             InitializeComponent();
+            PagesContainer.Content = new AlgorithmEditor();
+            
         }
 
         private void TabButton_Click(object sender, RoutedEventArgs e)
@@ -39,13 +42,42 @@ namespace PM_Studio
                     break;
 
                 case 2:
-
+                 
                     break;
                 case 3:
-
+                    
                     break;
             }
         }
 
+        private void btnCollapsePanel_Click(object sender, RoutedEventArgs e)
+        {
+            //If the Panel was not collapsed, clear the text of the buttons, and make the buttons panel has auto width
+            //and make the frames panel take the rest space(collapsing the panel)
+            //then mark the IsPanelCollapsed as true
+            if(IsPanelCollapsed == false)
+            {
+                btnAlgorithm.Content = "A";
+                btnSchedule.Content = "S";
+                btnPublish.Content = "P";
+                btnTeam.Content = "T";
+                TabButtonsColumn.Width = new GridLength(1, GridUnitType.Auto);
+                TabesColumn.Width = new GridLength(1, GridUnitType.Star);
+                IsPanelCollapsed = true;
+            }
+            //If the panel was collapsed, set the buttons text to the original text
+            //and reset the panels back to their orginal width
+            //then mark the IsPanelCollapsed as false
+            else if(IsPanelCollapsed == true)
+            {
+                btnAlgorithm.Content = "Algorithm";
+                btnSchedule.Content = "Schedule";
+                btnPublish.Content = "Publish";
+                btnTeam.Content = "Team";
+                TabButtonsColumn.Width = new GridLength(1, GridUnitType.Star);
+                TabesColumn.Width = new GridLength(4, GridUnitType.Star);
+                IsPanelCollapsed = false;
+            }
+        }
     }
 }
