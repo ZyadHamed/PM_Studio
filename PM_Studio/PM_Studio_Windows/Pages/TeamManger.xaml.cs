d59cae7f0f688b27dd2314c8e3408749b11fa0d3
ZyadHamed <zyadhamedashour@gmail.com>
2020-08-27 19:07:58 +0200

    Added the Team Manger page
    Added the Team Manger page to be the main page for project team mangement

---
+++ PM_Studio/PM_Studio_Windows/Pages/TeamManger.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PM_Studio
+{
+    /// <summary>
+    /// Interaction logic for TeamManger.xaml
+    /// </summary>
+    public partial class TeamManger : Page
+    {
+        Team team;
+        public TeamManger(Team _team)
+        {
+            team = _team;
+            InitializeComponent();
+        }
+    }
+}
