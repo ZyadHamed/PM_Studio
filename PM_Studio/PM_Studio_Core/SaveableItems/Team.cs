aa10e54127d0715be86144f6f5c9cd708e36c71e
ZyadHamed <zyadhamedashour@gmail.com>
2020-08-27 19:11:40 +0200

    Added the Team and the TeamMember Savable Classes

---
+++ PM_Studio/PM_Studio_Core/SaveableItems/Team.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM_Studio
+{
+    [Serializable]
+    public class Team
+    {
+        public string TeamName { get; set; }
+        public List<TeamMember> TeamMembers { get; set; }
+    }
+}
---
+++ PM_Studio/PM_Studio_Core/SaveableItems/TeamMember.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM_Studio
+{
+    public class TeamMember
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public List<string> Tasks { get; set; }
+    }
+}
