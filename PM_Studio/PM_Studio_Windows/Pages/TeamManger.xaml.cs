0ab88132f733e491f0d27169d38b39967da4c744
ZyadHamed <zyadhamedashour@gmail.com>
2020-08-28 12:47:52 +0200

    Updated the TeamManger Page and added sample team for testing
    Updated the TeamManger Page and added the ability to switch between team members and added sample team for testing

--- PM_Studio/PM_Studio_Windows/Pages/TeamManger.xaml.cs
+++ PM_Studio/PM_Studio_Windows/Pages/TeamManger.xaml.cs
@@ -18,11 +18,42 @@ namespace PM_Studio
     /// </summary>
     public partial class TeamManger : Page
     {
-        Team team;
-        public TeamManger(Team _team)
+        TeamMangerViewModel teamMangerViewModel;
+        public TeamManger()
         {
-            team = _team;
             InitializeComponent();
+            List<TeamMember> teamMembers = new List<TeamMember>();
+            teamMembers.Add(new TeamMember()
+            {
+                Name = "Ahmed",
+                Job = "Senior Programmer",
+                Tasks = new List<string>(){ "Revise the code", "Help his team" }
+            });
+            teamMembers.Add(new TeamMember()
+            {
+                Name = "Ali",
+                Job = "Junior Programmer",
+                Tasks = new List<string>() { "Write Some Basic Code", "Do some basic tasks" }
+            });
+            teamMembers.Add(new TeamMember()
+            {
+                Name = "Kamel",
+                Job = "Game Designer",
+                Tasks = new List<string>() { "Plan how the game will go", "Design the enviroment of the game", "block out the base of the game" }
+            });
+            Team team = new Team()
+            {
+                TeamName = "The ROcks",
+                TeamMembers = teamMembers
+            };
+            teamMangerViewModel = new TeamMangerViewModel(team);
+            lstTeamMembers.ItemsSource = teamMangerViewModel.TeamMembers;
+            
+        }
+
+        private void lstTeamMembers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            lstTeamMembersTasks.ItemsSource = ((TeamMember)(e.AddedItems[0] as TeamMemberBlock).Tag).Tasks
         }
     }
 }
