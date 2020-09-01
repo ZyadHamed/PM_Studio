6e0fd1af3b61cb44d0c5b59384e54d8fb5349721
ZyadHamed <zyadhamedashour@gmail.com>
2020-09-01 14:52:07 +0200

    Fixed Bugs and Updated Properties in TeamMangerViewModel
    Fixed Bugs at setting properties in the Class and added more logic in Properties that are affected by other properties and fixed the return of some properties

--- PM_Studio/PM_Studio_Windows/ViewModels/TeamMangerViewModel.cs
+++ PM_Studio/PM_Studio_Windows/ViewModels/TeamMangerViewModel.cs
@@ -1,27 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq.Expressions;
-using System.Security.Policy;
-using System.Text;
-using System.Windows.Navigation;
+using System.Linq;
 
 namespace PM_Studio
 {
-    class TeamMangerViewModel
+   public class TeamMangerViewModel
     {
         #region Variables
+
         Team team;
+        string teamFilePath = "";
+        SaveLoadSystemViewModel saveLoadSystemViewModel = new SaveLoadSystemViewModel(null);
+
+        //Properties private Variables
+        private List<TeamMemberBlock> _TeamMembers;
+        private TeamMember _SelectedMember;
+        private List<string> _MemberTasks;
+
         #endregion
 
         #region Constructor
-        public TeamMangerViewModel(Team _team)
+        public TeamMangerViewModel(Team _team, string _teamFilePath)
         {
             team = _team;
+            teamFilePath = _teamFilePath;
+           
         }
         #endregion
 
         #region Methods
+
         public List<TeamMemberBlock> GetTeamMembers()
         {
             //Make a List of TeamMemberBlock to be the Container of the Data of the Team Member
@@ -51,10 +59,44 @@ namespace PM_Studio
             return FileManger.GetAllFilesByExtension(filePath, ".team");
         }
 
+        /// <summary>
+        /// Saves the current data of the team inside the teamFilepath
+        /// </summary>
+        void SaveTeamData()
+        {
+            saveLoadSystemViewModel.Save(teamFilePath, team);
+        }
+
+        /// <summary>
+        /// Removes a TeamMember From the Team
+        /// </summary>
+        /// <param name="teamMember">The TeamMember to remove</param>
+        public void RemoveTeamMember(TeamMember teamMember)
+        {
+            int i = 0;
+            //Loop inside all TeamMembers
+            foreach(TeamMember member in team.TeamMembers)
+            {
+                //If the data of the current teamMember in the Loop Matches the data of the given TeamMember
+                if(member.Name == teamMember.Name && member.Job == teamMember.Job && Enumerable.SequenceEqual(member.Tasks, teamMember.Tasks))
+                {
+                    //Remove that teamMember using the index of him
+                    team.TeamMembers.RemoveAt(i);
+                    //Break the Operation
+                    break;
+                }
+                i++;
+            }
+            //Set the TeamMember property to the TeamMembers of the team
+            TeamMembers = GetTeamMembers();
+            //Save the current data of the team
+            SaveTeamData();
+        }
 
         #endregion
 
         #region Properties
+
         /// <summary>
         /// The TeamMembers inside the Team
         /// </summary>
@@ -62,30 +104,36 @@ namespace PM_Studio
         {
             get
             {
-                //return a List of TeamMembers from the GetTeamMembers Method
-                return GetTeamMembers();
+                //Set the private variable to the TeamMembers of the team
+                _TeamMembers =  GetTeamMembers();
+                //Return the private variable
+                return _TeamMembers;
             }
             set
             {
-                //Set the property to the incoming value
-                TeamMembers = value;
+                //Set the private variable to the incoming value
+                _TeamMembers = value;
             }
         }
 
         /// <summary>
-        /// The Tasks of an Indvdiual Member in the Team
+        /// The Tasks of the Selected Member in the Team
         /// </summary>
         public List<string> MemberTasks
         {
             get
             {
-                //Return null as an intial return (will be changed later)
-                return null;
+               //Set The Value of the private MemberTasks Variable to the SelectedMember Tasks
+                _MemberTasks = SelectedMember.Tasks;
+                //return the value of that private variable 
+                return _MemberTasks;
             }
             set
             {
-                //Set the property to the incoming value
-                MemberTasks = value;
+                //Set the private variable to the incoming Tasks
+                _MemberTasks = value;
+                //Set the Tasks of the Selected Member to the incoming Tasks
+                SelectedMember.Tasks = value;
             }
         }
 
@@ -105,6 +153,25 @@ namespace PM_Studio
                 team = value;
                 //Get All teamMembers inside that new team
                 TeamMembers = GetTeamMembers();
+            }
+        }
+
+        /// <summary>
+        /// The Current Member in Memory
+        /// </summary>
+        public TeamMember SelectedMember
+        {
+            get
+            {
+                //Return the private variable holding the Selected Member
+                return _SelectedMember;
+            }
+            set
+            {
+                //Set the private selected member to the incoming value
+                _SelectedMember = value;
+                //Set the MemberTasks to the tasks of the current member
+                MemberTasks = value.Tasks;
             }
         }
 
