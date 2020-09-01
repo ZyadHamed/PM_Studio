8f55b9c6755b6dcecd10d2b1c1ce0e7eb5165e3b
ZyadHamed <zyadhamedashour@gmail.com>
2020-09-01 15:02:12 +0200

    Added the ability to load teams from files in TeamManger
    Added the ability to load teams from files in TeamManger, added more dynamic code that gets all the Data from the TeamMangerViewModel, added some sample code for creating and testing a "team" file

--- PM_Studio/PM_Studio_Windows/Pages/TeamManger.xaml.cs
+++ PM_Studio/PM_Studio_Windows/Pages/TeamManger.xaml.cs
@@ -19,45 +19,72 @@ namespace PM_Studio
     public partial class TeamManger : Page
     {
         #region Variables
-        TeamMangerViewModel teamMangerViewModel;
+        public TeamMangerViewModel teamMangerViewModel;
+        SaveLoadSystemViewModel saveLoadSystemViewModel = new SaveLoadSystemViewModel(null);
         #endregion
 
         #region Constructor
         public TeamManger()
         {
             InitializeComponent();
-            //Define a List of teamMembers
-            List<TeamMember> teamMembers = new List<TeamMember>();
-            //Add Some TeamMembers to that List for testing
-            teamMembers.Add(new TeamMember()
-            {
-                Name = "Ahmed",
-                Job = "Senior Programmer",
-                Tasks = new List<string>(){ "Revise the code", "Help his team" }
-            });
-            teamMembers.Add(new TeamMember()
-            {
-                Name = "Ali",
-                Job = "Junior Programmer",
-                Tasks = new List<string>() { "Write Some Basic Code", "Do some basic tasks" }
-            });
-            teamMembers.Add(new TeamMember()
-            {
-                Name = "Kamel",
-                Job = "Game Designer",
-                Tasks = new List<string>() { "Plan how the game will go", "Design the enviroment of the game", "block out the base of the game" }
-            });
-            //Make a Team From that List of TeamMembers and give that Team a Name
-            Team team = new Team()
-            {
-                TeamName = "The ROcks",
-                TeamMembers = teamMembers
-            };
+           
             //Intilaize the TeamMangerViewModel Class with the team paramerter
-            teamMangerViewModel = new TeamMangerViewModel(team);
+            teamMangerViewModel = new TeamMangerViewModel(saveLoadSystemViewModel.GetTeam(@"E:\zyadhamedashour\team1.team"), @"E:\zyadhamedashour\team1.team");
             //Set the Item Source of the TeamMembers ListView to the TeamMembers from the View Model
             lstTeamMembers.ItemsSource = teamMangerViewModel.TeamMembers;
-            
+
+            // the Code that was used for testing the Functionallity of teamfiles:
+
+            ////1-Create a List of TeamMembers
+            // List<TeamMember> teamMembers = new List<TeamMember>();
+
+            ////2-Add Some TeamMembers to that List
+            //teamMembers.Add(new TeamMember()
+            //{
+            //    Name = "Ahmed",
+            //    Job = "Senior Programmer",
+            //    Tasks = new List<string>() { "Revise the code", "Help his team" }
+            //});
+            //teamMembers.Add(new TeamMember()
+            //{
+            //    Name = "Ali",
+            //    Job = "Junior Programmer",
+            //    Tasks = new List<string>() { "Write Some Basic Code", "Do some basic tasks" }
+            //});
+            //teamMembers.Add(new TeamMember()
+            //{
+            //    Name = "Kamel",
+            //    Job = "Game Designer",
+            //    Tasks = new List<string>() { "Plan how the game will go", "Design the enviroment of the game", "block out the base of the game" }
+            //});
+            //teamMembers.Add(new TeamMember()
+            //{
+            //    Name = "Basel",
+            //    Job = "Senior Designer",
+            //    Tasks = new List<string>() { "Revise the work of the Game Designers", "Design the details of enviroment of the game", "Design the characters" }
+            //});
+
+            ////Make a Team From that List of TeamMembers and give that Team a Name
+            //Team team = new Team()
+            //{
+            //    TeamName = "The Devs",
+            //    TeamMembers = teamMembers
+            //};
+
+            ////Save that Team In a Path
+            //saveLoadSystemViewModel.Save(@"E:\zyadhamedashour\team1.team", team);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void ReloadTeamMembers()
+        {
+            //Empty the TeamMembers ListView
+            lstTeamMembers.ItemsSource = null;
+            //Set the Item Source of the TeamMembers ListView to the TeamMembers from the View Model
+            lstTeamMembers.ItemsSource = teamMangerViewModel.TeamMembers;
         }
 
         #endregion
@@ -65,8 +92,9 @@ namespace PM_Studio
         #region Events
         private void lstTeamMembers_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            teamMangerViewModel.SelectedMember = ((TeamMember)(e.AddedItems[0] as TeamMemberBlock).Tag);
             //Get the Tasks from the selected Team Member and Display it in the TeamMemberTasks ListView
-            lstTeamMembersTasks.ItemsSource = ((TeamMember)(e.AddedItems[0] as TeamMemberBlock).Tag).Tasks;
+            lstTeamMembersTasks.ItemsSource = teamMangerViewModel.MemberTasks;
         }
 
         #endregion
