42186fecfc1058ab94eb1bc5b01b361b7d55ff47
ZyadHamed <zyadhamedashour@gmail.com>
2020-09-15 00:10:19 +0200

    Added the Shedule and the Task Savable Items
    Added the Shedule class for handling tasks and timing of the project

---
+++ PM_Studio/PM_Studio_Core/SaveableItems/Shedule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM_Studio
+{
+    [Serializable]
+    public class Shedule
+    {
+
+        public List<Task> Tasks { get; set; }
+
+    }
+}
---
+++ PM_Studio/PM_Studio_Core/SaveableItems/Task.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM_Studio
+{
+    public class Task
+    {
+
+        #region Variables
+
+        private string _TaskProgress = "Undone";
+        private List<TeamMember> _TaskWorkers = new List<TeamMember>();
+
+        #endregion
+
+        #region Constructor
+        public Task(string _TaskTitle, string _TaskDescription, string TaskDuration)
+        {
+            TaskTitle = _TaskTitle;
+            TaskDescription = _TaskDescription;
+        }
+        #endregion
+
+        #region Methods
+
+        public void AddTaskWorker(TeamMember TaskWorker)
+        {
+            TaskWorkers.Add(TaskWorker);
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string TaskProgress
+        {
+            get
+            {
+                return _TaskProgress;
+            }
+
+            set
+            {
+                _TaskProgress = value;
+            }
+        }
+
+        public List<TeamMember> TaskWorkers
+        {
+            get
+            {
+                return _TaskWorkers;
+            }
+            set
+            {
+                _TaskWorkers = value;
+            }
+        }
+
+        public string TaskTitle {get; set;}
+        public string TaskDescription { get; set; }
+
+        #endregion
+
+    }
+}
