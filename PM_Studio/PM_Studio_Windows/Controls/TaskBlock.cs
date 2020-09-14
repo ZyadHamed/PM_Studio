dd1cbedb8cf1493bb12cb89d2e50ad28dfe6c38b
ZyadHamed <zyadhamedashour@gmail.com>
2020-09-15 00:12:14 +0200

    Added the Core of the Task Block Class
    Added the Core of the Task Block Class that is responsible for visualizing Tasks of the Schedule to the user

---
+++ PM_Studio/PM_Studio_Windows/Controls/TaskBlock.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace PM_Studio
+{
+    public class TaskBlock : Border
+    {
+
+        #region Variables
+
+        private Task _Task;
+
+        #endregion
+
+        #region Constructor
+
+        public TaskBlock(Task task)
+        {
+            Task = task;
+        }
+
+        #endregion
+
+        Task Task
+        {
+            get
+            {
+                return _Task;
+            }
+            set
+            {
+                _Task = value;
+            }
+        }
+
+    }
+}
