41cfb4ea7f8862b2025b96c71e79f773e371a018
ZyadHamed <zyadhamedashour@gmail.com>
2020-09-17 22:20:57 +0200

    Added the SheduleMangerViewModel Class
    Added the SheduleMangerViewModel Class to handle the connection between tasks and the UI

---
+++ PM_Studio/PM_Studio_Windows/ViewModels/SheduleMangerViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM_Studio
+{
+    class SheduleMangerViewModel
+    {
+
+        #region Methods
+
+        public (List<TaskBlock> UndoneTasks, List<TaskBlock> InProgrssTasks, List<TaskBlock> DoneTasks) SortTasks(List<TaskBlock> TaskBlocks)
+        {
+            List<TaskBlock> UndoneTasks = new List<TaskBlock>();
+            List<TaskBlock> InProgrssTasks = new List<TaskBlock>();
+            List<TaskBlock> DoneTasks = new List<TaskBlock>();
+            foreach(TaskBlock taskBlock in TaskBlocks)
+            {
+                if(taskBlock.Task.TaskProgress == "Undone")
+                {
+                    UndoneTasks.Add(taskBlock);
+                }
+                else if (taskBlock.Task.TaskProgress == "In Progress")
+                {
+                    InProgrssTasks.Add(taskBlock);
+                }
+                else if (taskBlock.Task.TaskProgress == "Done")
+                {
+                    DoneTasks.Add(taskBlock);
+                }
+            }
+            return (UndoneTasks, InProgrssTasks, DoneTasks);
+        }
+
+        #endregion
+
+    }
+}
