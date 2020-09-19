b218a4948c3f9ce11ead38927189344b775832d4
ZyadHamed <zyadhamedashour@gmail.com>
2020-09-19 12:45:52 +0200

    Updated the SheduleManger to match the new changes in the SheduleMangerViewModel

--- PM_Studio/PM_Studio_Windows/Pages/SheduleManger.xaml.cs
+++ PM_Studio/PM_Studio_Windows/Pages/SheduleManger.xaml.cs
@@ -18,10 +18,15 @@ namespace PM_Studio
     /// </summary>
     public partial class SheduleManger : Page
     {
+
+       
+
+        #region Constructor
         public SheduleManger()
         {
             InitializeComponent();
-            //Code Used for testing the tasks and it's sorting
+
+            ////Code Used for Creating a shedule file
             //Task task1 = new Task("Design the Form", "", "From 9/9/2020 To 15/12/2020");
             //task1.TaskProgress = "Done";
             //Task task2 = new Task("Program the front End of the Form", "", "From 8/8/2020 To 8/9/2020");
@@ -32,18 +37,28 @@ namespace PM_Studio
             //task4.TaskProgress = "Undone";
             //Task task5 = new Task("Publish the Program", "", "From 5/1/2021 To 15/1/2021");
             //task5.TaskProgress = "Undone";
-            //List<TaskBlock> tasks = new List<TaskBlock>();
-            //tasks.Add(new TaskBlock(task1));
-            //tasks.Add(new TaskBlock(task2));
-            //tasks.Add(new TaskBlock(task3));
-            //tasks.Add(new TaskBlock(task4));
-            //tasks.Add(new TaskBlock(task5));
-            //SheduleMangerViewModel sheduleMangerViewModel = new SheduleMangerViewModel();
+            //List<Task> tasks = new List<Task>();
+            //tasks.Add(task1);
+            //tasks.Add(task2);
+            //tasks.Add(task3);
+            //tasks.Add(task4);
+            //tasks.Add(task5);
+            //Shedule shedule = new Shedule()
+            //{
+            //    Name = "PMShedule.pmshed",
+            //    Tasks = tasks
+            //};
+            //SaveLoadSystemViewModel saveLoadSystemViewModel = new SaveLoadSystemViewModel(null);
+            //saveLoadSystemViewModel.Save((@"E:\zyadhamedashour\" + shedule.Name, shedule));
+            SheduleMangerViewModel sheduleMangerViewModel = new SheduleMangerViewModel(@"E:\zyadhamedashour\PMShedule.pmshed");
 
-            //tviUndone.ItemsSource = sheduleMangerViewModel.SortTasks(tasks).UndoneTasks;
-            //tviInProgress.ItemsSource = sheduleMangerViewModel.SortTasks(tasks).InProgrssTasks;
-            //tviDone.ItemsSource = sheduleMangerViewModel.SortTasks(tasks).DoneTasks;
-            
+            tviUndone.ItemsSource = sheduleMangerViewModel.UndoneTasks;
+            tviInProgress.ItemsSource = sheduleMangerViewModel.InProgressTasks;
+            tviDone.ItemsSource = sheduleMangerViewModel.DoneTasks;
+
         }
+
+        #endregion
+
     }
 }
