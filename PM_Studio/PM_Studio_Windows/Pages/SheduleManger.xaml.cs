45930ebcd50c13c536da7b3ea4ef058744861ad8
ZyadHamed <zyadhamedashour@gmail.com>
2020-09-20 18:47:41 +0200

    Added response in Case of no shedules in the SheduleManger
    Added a resoponse that asks the user to create a Shedule in Case there is none in the project folder
    added a button for Creating Tasks inside the SheduleManger

--- PM_Studio/PM_Studio_Windows/Pages/SheduleManger.xaml.cs
+++ PM_Studio/PM_Studio_Windows/Pages/SheduleManger.xaml.cs
@@ -18,24 +18,23 @@ namespace PM_Studio
     /// </summary>
     public partial class SheduleManger : Page
     {
-
-       
-
+        SheduleMangerViewModel sheduleMangerViewModel = new SheduleMangerViewModel(@"E:\zyadhamedashour\PMShedule.pmshed");
+        //Button btnAddTask = new Button();
         #region Constructor
         public SheduleManger()
         {
             InitializeComponent();
 
             ////Code Used for Creating a shedule file
-            //Task task1 = new Task("Design the Form", "", "From 9/9/2020 To 15/12/2020");
+            //Task task1 = new Task("A", "", "From 9/9/2020 To 15/12/2020");
             //task1.TaskProgress = "Done";
-            //Task task2 = new Task("Program the front End of the Form", "", "From 8/8/2020 To 8/9/2020");
+            //Task task2 = new Task("B", "", "From 8/8/2020 To 8/9/2020");
             //task2.TaskProgress = "Done";
-            //Task task3 = new Task("Revise the Code", "", "From 9/10/2020 To 5/11/2020");
+            //Task task3 = new Task("C", "", "From 9/10/2020 To 5/11/2020");
             //task3.TaskProgress = "In Progress";
-            //Task task4 = new Task("Fix Bugs", "", "From 12/12/2020 To 5/1/2021");
+            //Task task4 = new Task("D", "", "From 12/12/2020 To 5/1/2021");
             //task4.TaskProgress = "Undone";
-            //Task task5 = new Task("Publish the Program", "", "From 5/1/2021 To 15/1/2021");
+            //Task task5 = new Task("E", "", "From 5/1/2021 To 15/1/2021");
             //task5.TaskProgress = "Undone";
             //List<Task> tasks = new List<Task>();
             //tasks.Add(task1);
@@ -45,20 +44,63 @@ namespace PM_Studio
             //tasks.Add(task5);
             //Shedule shedule = new Shedule()
             //{
-            //    Name = "PMShedule.pmshed",
+            //    Name = "PMShedule2.pmshed",
             //    Tasks = tasks
             //};
             //SaveLoadSystemViewModel saveLoadSystemViewModel = new SaveLoadSystemViewModel(null);
-            //saveLoadSystemViewModel.Save((@"E:\zyadhamedashour\" + shedule.Name, shedule));
-            SheduleMangerViewModel sheduleMangerViewModel = new SheduleMangerViewModel(@"E:\zyadhamedashour\PMShedule.pmshed");
+            //saveLoadSystemViewModel.Save(@"E:\zyadhamedashour\" + shedule.Name, shedule);
+            if (FileManger.GetAllFilesByExtension(@"E:\zyadhamedashour", ".pmshed").Count < 1)
+            {
+                NoSheduleGrid.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                tvTasks.Visibility = Visibility.Visible;
+                FillTreeView();
 
-            tviUndone.ItemsSource = sheduleMangerViewModel.UndoneTasks;
-            tviInProgress.ItemsSource = sheduleMangerViewModel.InProgressTasks;
-            tviDone.ItemsSource = sheduleMangerViewModel.DoneTasks;
+            }
+           
+            
 
+        }
+
+        private void btnAddTask_Click(object sender, RoutedEventArgs e)
+        {
+            Create_ModifyItemsWindow CreateWindow = new Create_ModifyItemsWindow(2);
+            CreateWindow.lbDataField1Text = "Task Title: ";
+            CreateWindow.lbDataField3Text = "Task Duration: ";
+            if(CreateWindow.ShowDialog() == true)
+            {
+                sheduleMangerViewModel.AddTask(new Task(CreateWindow.txtDataField1Text, "", CreateWindow.txtDataField3Text)
+                {
+                    TaskProgress = "Undone"
+                });
+                FillTreeView();
+            }
+            
         }
 
         #endregion
 
+        #region Methods
+
+        void FillTreeView()
+        {
+            tviUndone.ItemsSource = null;
+            tviInProgress.ItemsSource = null;
+            tviDone.ItemsSource = null;
+
+            tviUndone.Items.Clear();
+            tviInProgress.Items.Clear();
+            tviDone.Items.Clear();
+
+            tviUndone.ItemsSource = sheduleMangerViewModel.UndoneTasks;
+
+            tviInProgress.ItemsSource = sheduleMangerViewModel.InProgressTasks;
+            
+            tviDone.ItemsSource = sheduleMangerViewModel.DoneTasks;
+        }
+
+        #endregion
     }
 }
