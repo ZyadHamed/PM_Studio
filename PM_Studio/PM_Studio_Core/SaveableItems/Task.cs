bdcb92ce5b38b59a35394da833511ca679ed50ef
ZyadHamed <zyadhamedashour@gmail.com>
2020-09-21 17:23:04 +0200

    Updated the Task Class and Added New Properties
    Added the Start Date and the End Date Properties to replace the Task Duration
    And added a Method to Determine the progress of the Task according to the Starting and Ending Dates of It

--- PM_Studio/PM_Studio_Core/SaveableItems/Task.cs
+++ PM_Studio/PM_Studio_Core/SaveableItems/Task.cs
@@ -10,42 +10,84 @@ namespace PM_Studio
 
         #region Variables
 
-        private string _TaskProgress = "Undone";
+        private string _Progress = "Upcoming";
         private List<TeamMember> _TaskWorkers = new List<TeamMember>();
-        private string _TaskDuration;
+        private DateTime _StartDate;
+        private DateTime _EndDate;
+        private bool _IsDone;
 
         #endregion
 
         #region Constructor
-        public Task(string _TaskTitle, string _TaskDescription, string _TaskDuration)
+        public Task(string _TaskTitle, string _TaskDescription, DateTime _StartDate, DateTime _EndDate, bool _IsDone = false)
         {
             TaskTitle = _TaskTitle;
             TaskDescription = _TaskDescription;
-            TaskDuration = _TaskDuration;
+            StartDate = _StartDate;
+            EndDate = _EndDate;
+            IsDone = _IsDone;
+
+            SetTaskProgress();
         }
         #endregion
 
         #region Methods
 
+        /// <summary>
+        /// Adds a TeamMember that is working on that specific Task
+        /// </summary>
+        /// <param name="TaskWorker">The TeamMember that will work on the Task</param>
         public void AddTaskWorker(TeamMember TaskWorker)
         {
             TaskWorkers.Add(TaskWorker);
+        }
+
+        void SetTaskProgress()
+        {
+            //If the task was Done, set the Progress to Done
+            if (IsDone == true)
+            {
+                Progress = "Done";
+            }
+            //If not, check the progress according to the Dates of the Task
+            else
+            {
+                //If The Start Date of the Task was Later than Today, the Task is Upcoming
+                if (DateTime.Compare(DateTime.Now, StartDate) < 0)
+                {
+                    Progress = "Upcoming";
+                }
+                else
+                {
+                    //If the start Date has Already arrived but the end date is not, the Task is in Progress
+                    if (DateTime.Compare(DateTime.Now, StartDate) >= 0 && DateTime.Compare(DateTime.Now, EndDate) < 0)
+                    {
+                        Progress = "In Progress";
+                    }
+                    //If the Start Date has Already arrived and the End Date also arrived and the Task is not done yet, then the task is Undone
+                    else if (DateTime.Compare(DateTime.Now, StartDate) >= 0 && DateTime.Compare(DateTime.Now, EndDate) > 0)
+                    {
+                        Progress = "Undone";
+                    }
+                }
+            }
+            
         }
 
         #endregion
 
         #region Properties
 
-        public string TaskProgress
+        public string Progress
         {
             get
             {
-                return _TaskProgress;
+                return _Progress;
             }
 
             set
             {
-                _TaskProgress = value;
+                _Progress = value;
             }
         }
 
@@ -61,15 +103,41 @@ namespace PM_Studio
             }
         }
 
-        public string TaskDuration
+        public DateTime StartDate
         {
             get
             {
-                return _TaskDuration;
+                return _StartDate;
             }
             set
             {
-                _TaskDuration = value;
+                _StartDate = value;
+                SetTaskProgress();
+            }
+        }
+
+        public DateTime EndDate
+        {
+            get
+            {
+                return _EndDate;
+            }
+            set
+            {
+                _EndDate = value;
+                SetTaskProgress();
+            }
+        }
+
+        public bool IsDone
+        {
+            get
+            {
+                return _IsDone;
+            }
+            set
+            {
+              _IsDone = value;
             }
         }
 
