6c672d6a379ffdb13f7656a66aaa466d04dcb100
ZyadHamed <zyadhamedashour@gmail.com>
2020-09-24 20:18:50 +0200

    Added the DateTimeMethods Class for creating Extension Methods
    Added the DateTimeMethods Class for creating DateTime Extension Methods such as the GetTimeStamp() Method

---
+++ PM_Studio/PM_Studio_Core/DataAccessLayer/DateTimeMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM_Studio
+{
+    public static class DateTimeMethods
+    {
+        public static long GetTimeStamp(this DateTime dateTime)
+        {
+            Console.WriteLine(dateTime.Kind.ToString());
+            DateTimeOffset dateTimeOffset = dateTime.ToUniversalTime();
+            return dateTimeOffset.ToUnixTimeSeconds();
+        }
+    }
+}
