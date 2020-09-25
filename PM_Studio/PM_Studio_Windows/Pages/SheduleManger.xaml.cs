1c6996f8ae4667f3d8ab0139b1b9ff427cb12b93
ZyadHamed <zyadhamedashour@gmail.com>
2020-09-26 00:19:13 +0200

    Added testing for the exporting of the files as PDF

--- PM_Studio/PM_Studio_Windows/Pages/SheduleManger.xaml.cs
+++ PM_Studio/PM_Studio_Windows/Pages/SheduleManger.xaml.cs
@@ -64,7 +64,10 @@ namespace PM_Studio
                 FillTreeView();
             }
 
-
+            PDF_Exporter exporter = new PDF_Exporter();
+            SaveLoadSystemViewModel saveLoadSystemViewModel = new SaveLoadSystemViewModel(null);
+            List<Algorithm> algorithms = saveLoadSystemViewModel.GetAllAlgorithms(@"E:\zyadhamedashour");
+            exporter.GeneratePDFFile(@"E:\zyadhamedashour\", "Calculator2.pdf", algorithms);
 
         }
 
