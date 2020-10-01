6767d570dfc2352988ae4e7fcac803c64dbc9902
ZyadHamed <zyadhamedashour@gmail.com>
2020-10-01 16:41:28 +0200

    Updated the NodeEditorCanvas and added More Features
    Added the Ability to Delete NodeBlocks inside the Canvas
    Added a Grid to the Canvas and added ability to Toggle it On/Off using G key
    Updated the BackColor and the Arrows Color inside the Canvas
    Fixed Bugs releated to the Focusability of the Canvas
    Added More Comments to the Code

--- PM_Studio/PM_Studio_Windows/Controls/NodesEditorCanvas.cs
+++ PM_Studio/PM_Studio_Windows/Controls/NodesEditorCanvas.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -13,6 +12,7 @@ namespace PM_Studio
         List<string> blocks = new List<string>() { "Block1", "Block2", "Block3", "Block4", "Block5", "Block6" };
         List<NodeBlock> SelectedBlocks = new List<NodeBlock>();
 
+        private bool isGridVisible = false;
         #endregion
 
         #region Constructor
@@ -20,82 +20,12 @@ namespace PM_Studio
         public NodesEditorCanvas()
         {
             this.MouseDown += NodesEditorCanvas_MouseDown;
-            this.Background = Brushes.Blue;
+            this.KeyDown += NodesEditorCanvas_KeyDown;
+
+            this.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2E292C"));
+            this.Focusable = true;
 
             FillCanvas(blocks);
-
-        }
-
-        #endregion
-
-        #region Events
-
-        private void NodesEditorCanvas_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            //If there was at least 1 nodeblock in the Canvas, do the Selection
-            if (this.Children.Count > 0)
-            {
-                //If the User pressed Left Button
-                if (e.ChangedButton == MouseButton.Left)
-                {
-                    //Create a HitTestResult for storing the object in which the user has clicked
-                    HitTestResult r = VisualTreeHelper.HitTest(this, e.GetPosition(this));
-
-                    //If the user has not clicked a NodeBlock, then He has clicked on the Canvas, then Unselect all the NodeBlocks
-                    if (r.VisualHit.GetType() != typeof(NodeBlock))
-                    {
-                        UnSelectAllNodeBlocks();
-                    }
-
-                    //else if the User selected a NodeBlock and Was Holding Ctrl at the Same Time ,Select that Block and  Add it to the Selected Blocks List
-                    else if (r.VisualHit.GetType() == typeof(NodeBlock) && Keyboard.IsKeyDown(Key.LeftCtrl) == true)
-                    {
-                        //Get the NodeBlock that the user has Clicked
-                        NodeBlock block = r.VisualHit as NodeBlock;
-
-                        if(SelectedBlocks.Find(x => x == block) == null)
-                        {
-                            //Set the Property IsSelected to true
-                            block.IsSelected = true;
-
-                            //Add this Block to the SelectedBlocks List
-                            SelectedBlocks.Add(block);
-                        }
-                    }
-
-                    //Else if the User Selected a NodeBlock and Was Holding Alt at the Same Time, Unselect that Block and Remove it From the Selected Blocks List
-                    else if (r.VisualHit.GetType() == typeof(NodeBlock) && Keyboard.IsKeyDown(Key.LeftAlt) == true)
-                    {
-
-                        //Get the NodeBlock that the user has Clicked
-                        NodeBlock block = r.VisualHit as NodeBlock;
-
-                        //Set the Property IsSelected to false
-                        block.IsSelected = false;
-
-                        //Remove that Block from the SelectedBlocks List
-                        SelectedBlocks.Remove(block);
-
-                    }
-
-                    //If the user clicked a NodeBlock without clicking any other buttons, Unselect all NodeBlocks and Select this NodeBlock
-                    else if (r.VisualHit.GetType() == typeof(NodeBlock))
-                    {
-                        //Get the NodeBlock that the User has Selected
-                        NodeBlock block = r.VisualHit as NodeBlock;
-
-                        //UnSelect All NodeBlocks
-                        UnSelectAllNodeBlocks();
-
-                        //Set the Selection of the NodeBlock that the User has Clicked to true
-                        block.IsSelected = true;
-
-                        //Add that Block to the List of SelectedBlocks
-                        SelectedBlocks.Add(block);
-                    }
-
-                }
-            }
 
         }
 
@@ -110,9 +40,9 @@ namespace PM_Studio
         /// <param name="SecondBlock">The Second Block</param>
         void Connect(NodeBlock FirstBlock, NodeBlock SecondBlock)
         {
-            //Create the Arrow that will connect both blocks and set it's color to black
+            //Create the Arrow that will connect both blocks and set it's color to Ghost white and add some Stroke thickness
             Arrow arrow = new Arrow();
-            arrow.Stroke = Brushes.Black;
+            arrow.Stroke = Brushes.GhostWhite;
             arrow.StrokeThickness = 3;
 
             //Add that Arrow to the Canvas
@@ -186,6 +116,222 @@ namespace PM_Studio
             }
             //Clear the List of SelectedBlocks
             SelectedBlocks.Clear();
+        }
+
+        void DeleteSelectedNodeBlocks()
+        {
+            //Create a List of Arrows to Contain the Arrows that Shall be Removed
+            List<Arrow> arrowsToRemove = new List<Arrow>();
+
+            //Loop on each block in the SelectedBlocks List and Break It's RelationShip with the other Blocks
+            foreach (NodeBlock SelectedBlock in SelectedBlocks)
+            {
+                //Now Loop inside all the NodeBlocks in the Canvas
+                foreach (var Control in this.Children)
+                {
+                    if (Control.GetType() == typeof(NodeBlock))
+                    {
+                        //Create a NodeBlock that stores the Current NodeBlock in the Loop
+                        NodeBlock block = Control as NodeBlock;
+
+                        //Check if the FromArrows List in that block contains the ToArrow of the Current SelectedBlock in the Loop
+                        Arrow blockFromArrow = block.FromArrows.Find(x => x == SelectedBlock.ToArrow);
+
+                        //If the arrow was not null, then there was an arrow going from that Selected Block into the Current Block, then Remove that arrow and break the relationship
+                        if (blockFromArrow != null)
+                        {
+                            //Remove that arrow from the FromArrows of that Block
+                            block.FromArrows.Remove(blockFromArrow);
+
+                            //Add that arrow to the arrowsToRemove List
+                            arrowsToRemove.Add(blockFromArrow);
+                        }
+                        //Now Check if the ToArrow of the Current Block in the Loop Is a From arrow in the Current SelectedBlock in the Loop
+                        //(Check if there is a arrow that goes from the Current Block in the Loop into the Current SelectedBlock in the Loop)
+                        Arrow blockToArrow = SelectedBlock.FromArrows.Find(x => x == block.ToArrow);
+
+                        //If the arrow was not null, then the arrow that connects both blocks exist, then Remove that arrow and Break the relationship
+                        if (blockToArrow != null)
+                        {
+                            //Remove that arrow from the FromArrows of the Current SelectedBlock
+                            SelectedBlock.FromArrows.Remove(blockToArrow);
+
+                            //Add that arrow to the arrowsToRemove List
+                            arrowsToRemove.Add(blockToArrow);
+                        }
+                    }
+                }
+            }
+
+            //After Breaking all the RelationShips between the Selected NodeBlocks and the other Blocks, Delete the Unused Lines and the Selected Blocks
+
+            //Loop inside each arrow on the arrowsToRemove List
+            foreach (Arrow arrow in arrowsToRemove)
+            {
+                //Delete that arrow from the Canvas
+                this.Children.Remove(arrow);
+            }
+
+            //Now Loop Inside each block in the SelectedBlocks List
+            foreach (NodeBlock block1 in SelectedBlocks)
+            {
+                //Delete that Block From the Canvas
+                this.Children.Remove(block1);
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        private void NodesEditorCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //If there was at least 1 nodeblock in the Canvas, do the Selection
+            if (this.Children.Count > 0)
+            {
+                //If the User pressed Left Button
+                if (e.ChangedButton == MouseButton.Left)
+                {
+                    //Create a HitTestResult for storing the object in which the user has clicked
+                    HitTestResult r = VisualTreeHelper.HitTest(this, e.GetPosition(this));
+
+                    //If the user has not clicked a NodeBlock, then He has clicked on the Canvas, then Unselect all the NodeBlocks
+                    if (r.VisualHit.GetType() != typeof(NodeBlock))
+                    {
+                        UnSelectAllNodeBlocks();
+                        this.Focus();
+                    }
+
+                    //else if the User selected a NodeBlock and Was Holding Ctrl at the Same Time ,Select that Block and  Add it to the Selected Blocks List
+                    else if (r.VisualHit.GetType() == typeof(NodeBlock) && Keyboard.IsKeyDown(Key.LeftCtrl) == true)
+                    {
+                        //Get the NodeBlock that the user has Clicked
+                        NodeBlock block = r.VisualHit as NodeBlock;
+
+                        if(SelectedBlocks.Find(x => x == block) == null)
+                        {
+                            //Set the Property IsSelected to true
+                            block.IsSelected = true;
+
+                            //Add this Block to the SelectedBlocks List
+                            SelectedBlocks.Add(block);
+                        }
+                    }
+
+                    //Else if the User Selected a NodeBlock and Was Holding Alt at the Same Time, Unselect that Block and Remove it From the Selected Blocks List
+                    else if (r.VisualHit.GetType() == typeof(NodeBlock) && Keyboard.IsKeyDown(Key.LeftAlt) == true)
+                    {
+
+                        //Get the NodeBlock that the user has Clicked
+                        NodeBlock block = r.VisualHit as NodeBlock;
+
+                        //Set the Property IsSelected to false
+                        block.IsSelected = false;
+
+                        //Remove that Block from the SelectedBlocks List
+                        SelectedBlocks.Remove(block);
+
+                    }
+
+                    //If the user clicked a NodeBlock without clicking any other buttons, Unselect all NodeBlocks and Select this NodeBlock
+                    else if (r.VisualHit.GetType() == typeof(NodeBlock))
+                    {
+                        //Get the NodeBlock that the User has Selected
+                        NodeBlock block = r.VisualHit as NodeBlock;
+
+                        //UnSelect All NodeBlocks
+                        UnSelectAllNodeBlocks();
+
+                        //Set the Selection of the NodeBlock that the User has Clicked to true
+                        block.IsSelected = true;
+
+                        //Add that Block to the List of SelectedBlocks
+                        SelectedBlocks.Add(block);
+                    }
+
+                }
+            }
+
+        }
+
+        private void NodesEditorCanvas_KeyDown(object sender, KeyEventArgs e)
+        {
+            //If the User has Pressed G, Toggle the Grid Visibilty on and Off
+            if(e.Key == Key.G)
+            {
+                //If it was Visible, Set It's Visibilty to false
+                if(IsGridVisible == true)
+                {
+                    IsGridVisible = false;
+                }
+                //and Vise Versa
+                else
+                {
+                    IsGridVisible = true;
+                }
+            }
+
+            //If the User has Pressed Delete, Delete the Selected NodeBlocks
+            else if(e.Key == Key.Delete)
+            {
+                DeleteSelectedNodeBlocks();
+            }
+        }
+
+        #endregion
+
+
+
+        #region Properties
+
+        /// <summary>
+        /// The boolean which determines wheather the grid is visible or not
+        /// </summary>
+        public bool IsGridVisible
+        {
+            get
+            {
+                return isGridVisible;
+            }
+            set
+            {
+                isGridVisible = value;
+                //If the Visibilty of the Grid was True, then set the BackGround of the Canvas to a Grid BackGround
+                if (isGridVisible == true)
+                {
+                    this.Background = new DrawingBrush()
+                    {
+                        //Set the DrawingBrush TileMode to Tile to make sure the Grid Squares repeat
+                        TileMode = TileMode.Tile,
+                        Viewport = new System.Windows.Rect(-10, -10, 40, 40),
+
+                        //Set the ViewPort Units to Absoulute to Make sure Grid Size does not change with the window
+                        ViewportUnits = BrushMappingMode.Absolute,
+
+                        //Now Create the Geometry of the Grid Squares
+                        Drawing = new GeometryDrawing()
+                        {
+                            //Set the Geometry to a square with a area of 40x40
+                            Geometry = new RectangleGeometry() { Rect = new System.Windows.Rect(0, 0, 40, 40) },
+
+                            
+                            Pen = new Pen()
+                            {
+                                //Set the Color of the Grid Square to a dark gray Color
+                                Brush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2E292C")),
+
+                                //Set the thickness of the Lines forming the square to 1
+                                Thickness = 1
+                            }
+                        }
+                    };
+                }
+                else
+                {
+                    //If the IsGridVisible was false, set the BackGround Color to a Solid Dark Grey Color
+                    this.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#2E292C"));
+                }
+            }
         }
 
         #endregion
