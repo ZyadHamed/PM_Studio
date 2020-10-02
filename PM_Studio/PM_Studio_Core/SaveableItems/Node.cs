408abdd50fdb41fc1db054015358396f5c425b8c
ZyadHamed <zyadhamedashour@gmail.com>
2020-10-02 13:07:16 +0200

    Added the Node Savable Item
    Added the Node Savable Item to Store the Data of a NodeBlock and it's Connections

---
+++ PM_Studio/PM_Studio_Core/SaveableItems/Node.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PM_Studio
+{
+    [Serializable]
+    public class Node
+    {
+        public string Text { get; set; }
+
+        public string ToNodeText { get; set; }
+    }
+}
