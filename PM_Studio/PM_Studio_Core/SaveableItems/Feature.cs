6ca29168884ba1ba03bf09344e79dd83b3bcb660
ZyadHamed <zyadhamedashour@gmail.com>
2020-10-06 13:11:49 +0200

    Added the Stage and Feature Savable Classes
    Added the Stage and Feature Savable Classes To Create the Basic block for the StageEditor Page

---
+++ PM_Studio/PM_Studio_Core/SaveableItems/Feature.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM_Studio
+{
+    [Serializable]
+    public class Feature
+    {
+        public string Content { get; set; }
+    }
+}
---
+++ PM_Studio/PM_Studio_Core/SaveableItems/Stage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PM_Studio
+{
+    [Serializable]
+    public class Stage
+    {
+        public string Version { get; set; }
+        public string StageType { get; set; }
+        public List<Feature> Features { get; set; }
+        public List<string> BugsToFix { get; set; }
+    }
+}
