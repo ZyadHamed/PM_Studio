157aeb16be047550f18f13126e10614445cbf811
ZyadHamed <zyadhamedashour@gmail.com>
2021-01-28 05:36:45 +0200

    Updated the SheduleManger and added the creation of Shedule Button
    Updated the SheduleManger and added the creation of Shedule Button that shows when there isn't any schedule files in the project path.
    Added the logic to use the first schedule file found if they exist.
    Fixed bugs

--- PM_Studio/PM_Studio_Windows/Pages/SheduleManger.xaml.cs
+++ PM_Studio/PM_Studio_Windows/Pages/SheduleManger.xaml.cs
@@ -21,52 +21,20 @@ namespace PM_Studio
     /// </summary>
     public partial class SheduleManger : Page
     {
-        SheduleMangerViewModel sheduleMangerViewModel = new SheduleMangerViewModel(@"E:\zyadhamedashour\PMShedule2.pmshed");
+        SheduleMangerViewModel sheduleMangerViewModel;
+        SaveLoadSystemViewModel saveLoadSystemViewModel = new SaveLoadSystemViewModel(null);
         //Button btnAddTask = new Button();
+
         #region Constructor
+
         public SheduleManger()
         {
             InitializeComponent();
 
-            ////Code Used for Creating a shedule file
-            /////note: Code was Updated so you need to create a new SheduleFile so that the Tasks in it are affected with the Changes
-            //Task task1 = new Task("A", "", "From 9/9/2020 To 15/12/2020");
-            //task1.TaskProgress = "Done";
-            //Task task2 = new Task("B", "", "From 8/8/2020 To 8/9/2020");
-            //task2.TaskProgress = "Done";
-            //Task task3 = new Task("C", "", "From 9/10/2020 To 5/11/2020");
-            //task3.TaskProgress = "In Progress";
-            //Task task4 = new Task("D", "", "From 12/12/2020 To 5/1/2021");
-            //task4.TaskProgress = "Undone";
-            //Task task5 = new Task("E", "", "From 5/1/2021 To 15/1/2021");
-            //task5.TaskProgress = "Undone";
-            //List<Task> tasks = new List<Task>();
-            //tasks.Add(task1);
-            //tasks.Add(task2);
-            //tasks.Add(task3);
-            //tasks.Add(task4);
-            //tasks.Add(task5);
-            //Shedule shedule = new Shedule()
-            //{
-            //    Name = "PMShedule2.pmshed",
-            //    Tasks = new List<Task>()
-            //};
-            //SaveLoadSystemViewModel saveLoadSystemViewModel = new SaveLoadSystemViewModel(null);
-            //saveLoadSystemViewModel.Save(@"E:\zyadhamedashour\" + shedule.Name, shedule);
-
-            if (FileManger.GetAllFilesByExtension(@"E:\zyadhamedashour", ".pmshed").Count < 1)
-            {
-                NoSheduleGrid.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                tvTasks.Visibility = Visibility.Visible;
-                FillTreeView();
-            }
+            CheckShedule();
 
         }
 
-        
         #endregion
 
         #region Methods
@@ -94,6 +62,32 @@ namespace PM_Studio
 
             tviUndone.ItemsSource = sheduleMangerViewModel.UndoneTasks;
 
+        }
+
+        void CheckShedule()
+        {
+            //Make a list with all shedule files in the E: directory,(Will be changed soon to the path of the project)
+            List<string> SheduleFilePaths = FileManger.GetAllFilesByExtension(@"E:\", ".pmshed");
+            //If there wasn't any shedule files. show the message and the creation button for the user
+            if (SheduleFilePaths.Count < 1)
+            {
+                //Show the  creation of shedule page and hide the tasks grid
+                NoSheduleGrid.Visibility = Visibility.Visible;
+                tvTasks.Visibility = Visibility.Collapsed;
+            }
+
+            //If not, use the first file found as a default file opened.
+            else
+            {
+                //Get the first shedule file from the list
+                sheduleMangerViewModel = new SheduleMangerViewModel(SheduleFilePaths[0]);
+                
+                //Show the tasks page and hide the creation of shedule grid
+                tvTasks.Visibility = Visibility.Visible;
+                NoSheduleGrid.Visibility = Visibility.Collapsed;
+                //Fill the treeviews with tasks
+                FillTreeView();
+            }
         }
 
         #endregion
@@ -207,8 +201,19 @@ namespace PM_Studio
             
         }
 
+        private void btnCreateShedule_Click(object sender, RoutedEventArgs e)
+        {
+            Create_ModifyItemsWindow window = new Create_ModifyItemsWindow(1);
+            window.lbDataField2Text = "Schedule Name: ";
+            if(window.ShowDialog() == true)
+            {
+                saveLoadSystemViewModel.CreateSheduleFile(@"E:\", window.txtDataField2Text);
+            }
+            CheckShedule();
+        }
 
         #endregion
+
 
     }
 }
