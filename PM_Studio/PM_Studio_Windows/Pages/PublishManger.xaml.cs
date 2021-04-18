190f6499a401561c30d343b6f9d49c41c964ac85
ZyadHamed <zyadhamedashour@gmail.com>
2021-04-18 22:25:46 +0200

    Updated the PublishManger to match the changes in the PDF_Exporter

--- PM_Studio/PM_Studio_Windows/Pages/PublishManger.xaml.cs
+++ PM_Studio/PM_Studio_Windows/Pages/PublishManger.xaml.cs
@@ -3,13 +3,8 @@ using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
 
 namespace PM_Studio
 {
@@ -26,8 +21,13 @@ namespace PM_Studio
         private void btnExport_Click(object sender, RoutedEventArgs e)
         {
             PDF_Exporter exporter = new PDF_Exporter();
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if(saveFileDialog.ShowDialog() == true)
+            {
+                exporter.GeneratePDFFile(saveFileDialog.FileName + ".pdf", new PDFExportArgs() { ExportAlgorithms = true });
+            }
             
-            exporter.GeneratePDFFile(@"E:\", "Calculator2.pdf", new PDFExportArgs() { ExportAlgorithms = true });
+            
         }
     }
 }
