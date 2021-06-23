8703568d7b79f5a310b25dd7592053f080a2e361
ZyadHamed <zyadhamedashour@gmail.com>
2021-06-23 16:39:11 +0200

    Improved the design of the schedule manger and added new features
    The design of the schedule manager is now much better. Moreover, the new sorting types are now embedded into the design.

--- PM_Studio/PM_Studio_Windows/Pages/SheduleManger.xaml.cs
+++ PM_Studio/PM_Studio_Windows/Pages/SheduleManger.xaml.cs
@@ -30,7 +30,7 @@ namespace PM_Studio
         public SheduleManger()
         {
             InitializeComponent();
-
+            
             CheckShedule();
 
         }
@@ -42,25 +42,13 @@ namespace PM_Studio
         void FillTreeView()
         {
             //Set the ItemsSource of all the TreeNodes to null(to remove any Parents to the Items)
-            tviUpcomingTasks.ItemsSource = null;
-            tviInProgress.ItemsSource = null;
-            tviDone.ItemsSource = null;
-            tviUndone.ItemsSource = null;
-
-            //Clear all the Items inside all the TreeNodes
-            tviUpcomingTasks.Items.Clear();
-            tviInProgress.Items.Clear();
-            tviDone.Items.Clear();
-            tviUndone.Items.Clear();
+            lstCloseTasks.ItemsSource = null;
+            lstTasks.ItemsSource = null;
 
             //Reset the Items Source for each one of them
-            tviUpcomingTasks.ItemsSource = sheduleMangerViewModel.UpcomingTasks;
+            lstCloseTasks.ItemsSource = sheduleMangerViewModel.SortedByDeadLineTasks;
+            lstTasks.ItemsSource = sheduleMangerViewModel.SortedAlphabiticallyTasks;
 
-            tviInProgress.ItemsSource = sheduleMangerViewModel.InProgressTasks;
-
-            tviDone.ItemsSource = sheduleMangerViewModel.DoneTasks;
-
-            tviUndone.ItemsSource = sheduleMangerViewModel.UndoneTasks;
 
         }
 
@@ -73,7 +61,7 @@ namespace PM_Studio
             {
                 //Show the  creation of shedule page and hide the tasks grid
                 NoSheduleGrid.Visibility = Visibility.Visible;
-                tvTasks.Visibility = Visibility.Collapsed;
+                contentGrid.Visibility = Visibility.Collapsed;
             }
 
             //If not, use the first file found as a default file opened.
@@ -81,9 +69,9 @@ namespace PM_Studio
             {
                 //Get the first shedule file from the list
                 sheduleMangerViewModel = new SheduleMangerViewModel(SheduleFilePaths[0]);
-                
+
                 //Show the tasks page and hide the creation of shedule grid
-                tvTasks.Visibility = Visibility.Visible;
+                contentGrid.Visibility = Visibility.Visible;
                 NoSheduleGrid.Visibility = Visibility.Collapsed;
                 //Fill the treeviews with tasks
                 FillTreeView();
@@ -96,76 +84,76 @@ namespace PM_Studio
 
         private void menuItem_Click(object sender, RoutedEventArgs e)
         {
-            //If the Selected Item Was Not a Task Block, Don't do anything
-            if ((tvTasks.SelectedItem as TaskBlock) != null)
-            {
-                //If it was a Task Block, Store it in a Variable
-                TaskBlock SelectedItem = tvTasks.SelectedItem as TaskBlock;
+            ////If the Selected Item Was Not a Task Block, Don't do anything
+            //if ((tvTasks.SelectedItem as TaskBlock) != null)
+            //{
+            //    //If it was a Task Block, Store it in a Variable
+            //    TaskBlock SelectedItem = tvTasks.SelectedItem as TaskBlock;
 
-                //Get the Operation that the User Selected using the MenuItem's name
-                //If the User Selected Edit Task, Edit the Selected Task
-                if ((e.Source as MenuItem).Name == "menuItemEditTask")
-                {
-                    //Create a CreatingItem Window with 2 Data Fields(The Task Title and The Task Duration)
-                    Create_ModifyItemsWindow CreateWindow = new Create_ModifyItemsWindow(2, true);
+            //    //Get the Operation that the User Selected using the MenuItem's name
+            //    //If the User Selected Edit Task, Edit the Selected Task
+            //    if ((e.Source as MenuItem).Name == "menuItemEditTask")
+            //    {
+            //        //Create a CreatingItem Window with 2 Data Fields(The Task Title and The Task Duration)
+            //        Create_ModifyItemsWindow CreateWindow = new Create_ModifyItemsWindow(2, true);
 
-                    //Set the First Label Text to Task Title
-                    CreateWindow.lbDataField1Text = "Task Title: ";
+            //        //Set the First Label Text to Task Title
+            //        CreateWindow.lbDataField1Text = "Task Title: ";
 
-                    //Set the Second Label Text to Task Duration
-                    CreateWindow.lbDataField3Text = "Start Date: \nEndDate: ";
+            //        //Set the Second Label Text to Task Duration
+            //        CreateWindow.lbDataField3Text = "Start Date: \nEndDate: ";
 
-                    //Set the Title of the Window to Edit Task
-                    CreateWindow.Title = "Edit Task";
+            //        //Set the Title of the Window to Edit Task
+            //        CreateWindow.Title = "Edit Task";
 
-                    //Set the Text Inside the First TextBox to The Text of the Task Title
-                    CreateWindow.txtDataField1Text = SelectedItem.Task.Title;
+            //        //Set the Text Inside the First TextBox to The Text of the Task Title
+            //        CreateWindow.txtDataField1Text = SelectedItem.Task.Title;
 
-                    //Set the Start Date and the End Date Inside the Window to the Start and End Dates of the Task
-                    CreateWindow.StartDate = SelectedItem.Task.StartDate;
-                    CreateWindow.EndDate = SelectedItem.Task.EndDate;
+            //        //Set the Start Date and the End Date Inside the Window to the Start and End Dates of the Task
+            //        CreateWindow.StartDate = SelectedItem.Task.StartDate;
+            //        CreateWindow.EndDate = SelectedItem.Task.EndDate;
 
-                    //Show the Window as Dialog
-                    if (CreateWindow.ShowDialog() == true)
-                    {
-                        //Set the Data of the Task to the New Data in the Window
-                        SelectedItem.Task.Title = CreateWindow.txtDataField1Text;
-                        SelectedItem.Task.StartDate = CreateWindow.StartDate;
-                        SelectedItem.Task.EndDate = CreateWindow.EndDate;
+            //        //Show the Window as Dialog
+            //        if (CreateWindow.ShowDialog() == true)
+            //        {
+            //            //Set the Data of the Task to the New Data in the Window
+            //            SelectedItem.Task.Title = CreateWindow.txtDataField1Text;
+            //            SelectedItem.Task.StartDate = CreateWindow.StartDate;
+            //            SelectedItem.Task.EndDate = CreateWindow.EndDate;
 
-                        //Save the New Data of the Shedule(To save the changes Made in the Task)
-                        sheduleMangerViewModel.SaveSheduleData();
+            //            //Save the New Data of the Shedule(To save the changes Made in the Task)
+            //            sheduleMangerViewModel.SaveSheduleData();
 
-                        //Refill the TreeView to see the Changes
-                        FillTreeView();
-                    }
+            //            //Refill the TreeView to see the Changes
+            //            FillTreeView();
+            //        }
 
-                }
-                //If the User Selected Delete Task, Delete the Selected Task
-                else if ((e.Source as MenuItem).Name == "menuItemDeleteTask")
-                {
-                    //Remove the Task that is inside the TaskBlock from the Shedule
-                    sheduleMangerViewModel.Shedule.Tasks.Remove(SelectedItem.Task);
+            //    }
+            //    //If the User Selected Delete Task, Delete the Selected Task
+            //    else if ((e.Source as MenuItem).Name == "menuItemDeleteTask")
+            //    {
+            //        //Remove the Task that is inside the TaskBlock from the Shedule
+            //        sheduleMangerViewModel.Shedule.Tasks.Remove(SelectedItem.Task);
 
-                    //Save the New Data in the Shedule
-                    sheduleMangerViewModel.SaveSheduleData();
+            //        //Save the New Data in the Shedule
+            //        sheduleMangerViewModel.SaveSheduleData();
 
-                    //Refill the TreeView to see the Changes
-                    FillTreeView();
-                }
-                //If the User Selected Mark Task As Done, Mark the Selected Task as Done
-                else if ((e.Source as MenuItem).Name == "menuItemMarkTask")
-                {
-                    //Set the Stored Task in the TaskBlock as Done
-                    SelectedItem.Task.IsDone = true;
+            //        //Refill the TreeView to see the Changes
+            //        FillTreeView();
+            //    }
+            //    //If the User Selected Mark Task As Done, Mark the Selected Task as Done
+            //    else if ((e.Source as MenuItem).Name == "menuItemMarkTask")
+            //    {
+            //        //Set the Stored Task in the TaskBlock as Done
+            //        SelectedItem.Task.IsDone = true;
 
-                    //Save the New Data of the Shedule(To save the changes Made in the Task)
-                    sheduleMangerViewModel.SaveSheduleData();
+            //        //Save the New Data of the Shedule(To save the changes Made in the Task)
+            //        sheduleMangerViewModel.SaveSheduleData();
 
-                    //Refill the TreeView to see the Changes
-                    FillTreeView();
-                }
-            }
+            //        //Refill the TreeView to see the Changes
+            //        FillTreeView();
+            //    }
+            //}
 
         }
 
